20e7d8af6ba776e9955662a708b857e161851a90
worksasdisigned <weis.achim@gmail.com>
2026-03-21 22:30:25 +0100

    V2.93.0 bugfixes, pitstop, network stability

--- src/coordinator/WebHandler.cpp
+++ src/coordinator/WebHandler.cpp
@@ -7,7 +7,7 @@
 #include "StatsManager.h" 
 #include "WifiScanner.h"
 
-#define SYS_VER "v2.92.3" 
+#define SYS_VER "v2.93.0" 
 
 AsyncWebServer WebHandler::server(80);
 DNSServer WebHandler::dnsServer;
