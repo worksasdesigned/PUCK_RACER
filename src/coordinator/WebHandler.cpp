48223f536f30f89c5e3f8248f8a9414ffa1181a1
worksasdisigned <weis.achim@gmail.com>
2026-03-23 00:04:56 +0100

    V2.94.2 images, UI V2.0

--- src/coordinator/WebHandler.cpp
+++ src/coordinator/WebHandler.cpp
@@ -7,7 +7,7 @@
 #include "StatsManager.h" 
 #include "WifiScanner.h"
 
-#define SYS_VER "v2.93.0" 
+#define SYS_VER "v2.94.2" 
 
 AsyncWebServer WebHandler::server(80);
 DNSServer WebHandler::dnsServer;
